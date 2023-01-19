58db0d2291a415b7d1325b984e2ed29ce2a6bc23
xhctr <daw06.2023@gmail.com>
2023-01-19 09:21:05 +0100

    Probando Clase

--- src/calculaoragit/principal.java
+++ src/calculaoragit/principal.java
@@ -1,5 +1,9 @@
 package calculaoragit;
 
 public class principal {
-
+	public static void main(String[] args) {
+		calculadora miCalc = new calculadora();
+		System.out.println(miCalc.sumar(5,3));
+		System.out.println(miCalc.restar(5,3));
+		}
 }
