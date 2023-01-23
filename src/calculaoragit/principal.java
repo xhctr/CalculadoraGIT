5362b85ccb8cb6ca21fb1a2f435c4d62b9970a2a
xhctr <123063808+xhctr@users.noreply.github.com>
2023-01-23 12:26:22 +0100

    Funcion Dividir

--- src/calculaoragit/principal.java
+++ src/calculaoragit/principal.java
@@ -6,5 +6,6 @@ public class principal {
 		System.out.println(miCalc.sumar(5,3));
 		System.out.println(miCalc.restar(5,3));
 		System.out.println(miCalc.multiplicar(5,3));
+		System.out.println(miCalc.dividir(5,3));
 		}
 }
